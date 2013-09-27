4fd94ded019cfd68a11fafd233ed725aaf26a6c9
Zathos <derrin.boerschel@gmail.com>
2013-09-27 16:54:44 -0500

    Initial Creation

---
+++ UpdateAssemblies/UpdateAssemblies.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace UpdateAssemblies
+{
+    partial class UpdateAssemblies
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PickPathButton = new System.Windows.Forms.Button();
+            this.SelectProject = new System.Windows.Forms.ComboBox();
+            this.PathTextBox = new System.Windows.Forms.TextBox();
+            this.UpdateButton = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // PickPathButton
+            // 
+            this.PickPathButton.Location = new System.Drawing.Point(12, 65);
+            this.PickPathButton.Name = "PickPathButton";
+            this.PickPathButton.Size = new System.Drawing.Size(75, 23);
+            this.PickPathButton.TabIndex = 0;
+            this.PickPathButton.Text = "Path";
+            this.PickPathButton.UseVisualStyleBackColor = true;
+            this.PickPathButton.Click += new System.EventHandler(this.PickPathButton_Click);
+            // 
+            // SelectProject
+            // 
+            this.SelectProject.FormattingEnabled = true;
+            this.SelectProject.Location = new System.Drawing.Point(12, 12);
+            this.SelectProject.Name = "SelectProject";
+            this.SelectProject.Size = new System.Drawing.Size(260, 21);
+            this.SelectProject.TabIndex = 1;
+            this.SelectProject.SelectedIndexChanged += new System.EventHandler(this.SelectProject_SelectedIndexChanged);
+            // 
+            // PathTextBox
+            // 
+            this.PathTextBox.Location = new System.Drawing.Point(12, 39);
+            this.PathTextBox.Name = "PathTextBox";
+            this.PathTextBox.Size = new System.Drawing.Size(260, 20);
+            this.PathTextBox.TabIndex = 2;
+            // 
+            // UpdateButton
+            // 
+            this.UpdateButton.Location = new System.Drawing.Point(12, 94);
+            this.UpdateButton.Name = "UpdateButton";
+            this.UpdateButton.Size = new System.Drawing.Size(75, 23);
+            this.UpdateButton.TabIndex = 3;
+            this.UpdateButton.Text = "Update";
+            this.UpdateButton.UseVisualStyleBackColor = true;
+            this.UpdateButton.Click += new System.EventHandler(this.UpdateButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 127);
+            this.Controls.Add(this.UpdateButton);
+            this.Controls.Add(this.PathTextBox);
+            this.Controls.Add(this.SelectProject);
+            this.Controls.Add(this.PickPathButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button PickPathButton;
+        private System.Windows.Forms.ComboBox SelectProject;
+        private System.Windows.Forms.TextBox PathTextBox;
+        private System.Windows.Forms.Button UpdateButton;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ UpdateAssemblies/UpdateAssemblies.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace UpdateAssemblies
+{
+    public partial class UpdateAssemblies : Form
+    {
+        public UpdateAssemblies()
+        {
+            InitializeComponent();
+        }
+
+        private void PickPathButton_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                PathTextBox.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void UpdateButton_Click(object sender, EventArgs e)
+        {
+            //TODO use the path and SelectProject
+        }
+
+        private void SelectProject_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //ComboBoxItem item = new ComboBoxItem();
+
+            //SelectProject.Items.Add();
+        }
+    }
+}
